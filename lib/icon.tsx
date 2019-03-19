282a2bca3f0b3394407d4e6658d0215395f0640e
yangdepu <yangdepu@zdsoft.cn>
2019-03-19 11:08:54 +0800

    声明点击事件的类型

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -6,12 +6,15 @@ import './icon.scss';
 
 interface IconProps {
   name: string;
+  onClick: React.MouseEventHandler<SVGElement>;
+  //  声明onClick时，指定类型为React.MouseEventHandler，里面的e.target为SVG元素
+  // onClick: (e: React.MouseEvent) => void;
 }
 
 //  这个类型接受一个参数
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <svg className="golu-icon">
+    <svg className="golu-icon" {...props}>
       <use xlinkHref={`#${props.name}`}></use>
     </svg>
   )
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import Icon from './icon'
+const fn: React.MouseEventHandler = (e) => {
+  console.log(e);
+  console.log(e.target);
+  //  这里报错，因为target没有声明类型，所以这么写有可能会报错
+  // console.log(e.target.href)
+
+  //  如果想要打印e.target的属性，需要用断言明确e.target的类型
+  console.log((e.target as SVGUseElement).href)
+}
+
 
 ReactDOM.render(
   <div>
-    <Icon name="wechat" />
-    <Icon name="alipay" />
+    <Icon name="wechat" onClick={fn} />
+    <Icon name="alipay" onClick={fn} />
   </div>, document.querySelector('#root'))
