a6b6071df37bf5468ed6cf847dc8e7310aa93d7c
yangdepu <yangdepu@zdsoft.cn>
2019-04-01 16:46:53 +0800

    分开test和ci命令

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,16 @@
 module.exports = {
   verbose: true,
   clearMocks: false,
-  collectCoverage: true,
-  reporters: ["default","jest-junit"],
+  reporters: ["default"],
+  collectCoverage: false,
+
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
-  collectCoverage: true,
-  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
-  // globals: {
-  //   'ts-jest': {
-  //     tsConfig: 'tsconfig.test.json',
-  //   },
-  // },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
+  },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
     "\\.(scss|css|less|sass)$": "<rootDir>/test/__mocks__/object-mock.js",
