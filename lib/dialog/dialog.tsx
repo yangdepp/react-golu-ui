4aaa3a71386d087577a640d2125f33aa0f5121f2
yangdepu <yangdepu@zdsoft.cn>
2019-04-18 11:42:26 +0800

    dialog架子

---
+++ lib/dialog/dialog.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+interface Props {
+  visible: boolean;
+}
+
+const Dialog: React.FunctionComponent<Props> = props => {
+  return props.visible ? <div>dialog</div> : null;
+};
+
+export default Dialog;
