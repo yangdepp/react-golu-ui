69bea0112553b1d98352f6008a53414726aeb61d
yangdepp <965093975@qq.com>
2019-04-22 23:13:12 +0800

    init

--- lib/dialog/dialog.example1.tsx
+++ lib/dialog/dialog.example1.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import Dialog from './dialog1';
+import React, { useState } from "react";
+// import Dialog, { alert, confirm, modal } from "./dialog";
+import Dialog from "./dialog1";
+import { Button } from "../index";
 
-const ExampleDialog = () => {
-  return <Dialog />;
-};
+export default function() {
+  const [x1, setX1] = useState(false);
 
-export default ExampleDialog;
+  const handelCancel = () => {
+    console.log('do something');
+    setX1(false);
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>example1</h1>
+        <Button onClick={() => setX1(true)}>click1</Button>
+        <Dialog
+          visible={x1}
+          // onClose={() => setX1(false)}
+          okText="ok"
+          onOk={() => setX1(false)}
+          onCancel={() => handelCancel()}
+        />
+      </div>
+    </div>
+  );
+}
--- lib/dialog/dialog1.tsx
+++ lib/dialog/dialog1.tsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import React, { Fragment, ReactElement } from "react";
+import ReactDOM from "react-dom";
+import { Icon } from "../index";
+import { Button } from "../index";
+import "./dialog.scss";
+import { scopedClassMaker } from "../classes";
 
-const Dialog = () => {
-  return <div>Dialog</div>;
+interface Props {
+  visible: boolean;
+  maskClosable?: boolean;
+
+  footer?: Array<ReactElement>;
+  onOk?: React.MouseEventHandler;
+  onCancel: React.MouseEventHandler;
+  okText?: string;
+  cancelText?: string;
+  title?: string;
+
+  buttons?: Array<ReactElement>;
+  // onClose: React.MouseEventHandler;
+}
+
+const scopedClass = scopedClassMaker("golu-dialog");
+const sc = scopedClass;
+
+const Dialog: React.FunctionComponent<Props> = props => {
+  const onClickCancel: React.MouseEventHandler = e => {
+    props.onCancel(e);
+  };
+
+  // const onClickMask: React.MouseEventHandler = e => {
+  //   if (props.maskClosable) {
+  //     props.onClose(e);
+  //   } else {
+  //     return;
+  //   }
+  // };
+
+  const dialogDom = props.visible ? (
+    <Fragment>
+      <div className={sc("mask")} onClick={() => {}} />
+      <div className={sc()}>
+        <div className={sc("close")} onClick={() => {}}>
+          <Icon name="close" />
+        </div>
+        <header className={sc("header")}>提示</header>
+        <main className={sc("main")}>{props.children}</main>
+        {!props.footer && (
+          <footer className={sc("footer")}>
+            <Button onClick={onClickCancel}>
+              {props.cancelText || "取消"}
+            </Button>
+            <Button type="primary" onClick={() => {}}>
+              {props.okText || "确定"}
+            </Button>
+          </footer>
+        )}
+      </div>
+    </Fragment>
+  ) : null;
+
+  return ReactDOM.createPortal(dialogDom, document.body);
+};
+
+Dialog.defaultProps = {
+  maskClosable: true
 };
 
 export default Dialog;
