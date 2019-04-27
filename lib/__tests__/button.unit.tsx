18f85eea9f3d380d96d717eaab9ec2e20afa8e6b
yangdepp <965093975@qq.com>
2019-04-27 22:33:01 +0800

    init page

--- lib/__tests__/button.unit.tsx
+++
@@ -1,9 +0,0 @@
-import renderer from "react-test-renderer";
-import React from "react";
-import Button from "../button/button";
-describe("button tests", () => {
-  it("是个button", () => {
-    const json = renderer.create(<Button />).toJSON();
-    expect(json).toMatchSnapshot();
-  });
-});
--- lib/index.tsx
+++
@@ -1,7 +0,0 @@
-import "./index.scss";
-import Icon from "./icon/icon";
-import Button from "./button/button";
-
-
-export { Icon };
-export { Button };
