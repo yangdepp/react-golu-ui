7e22420a77419c15311d6346ad48672920ffc614
yangdepp <965093975@qq.com>
2019-04-27 22:18:39 +0800

    把官网单独打包

---
+++ webpack.config.doc.js
@@ -0,0 +1,19 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const base = require('./webpack.config');
+module.exports = Object.assign({}, base, {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'doc/lib')
+  },
+  entry: {
+    example: './example.tsx'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'GOLU',
+      template: 'example.html',
+      filename: 'example.html'
+    })
+  ]
+});
