5450ffaf8f5e3db8df4ebbb6d9830e33fd31ed1a
yangdepu <yangdepu@zdsoft.cn>
2019-10-25 17:29:06 +0800

    初步实现form表单

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,17 +1,58 @@
-import React, { useState } from 'react';
-import Form from './form';
+import React, { useState, Fragment } from 'react';
+import Form, { FormValue } from './form';
+import Validator, { noError } from './validator';
 
 const FormExample: React.FunctionComponent = () => {
-  const [formData] = useState({
-    username: '',
-    password: ''
+  const [formData, setFormData] = useState<FormValue>({
+    username: 'yang',
+    password: '',
   });
-
   const [fileds] = useState([
     { name: 'username', label: '用户名', input: { type: 'text' } },
-    { name: 'password', label: '密码', input: { type: 'password' } }
+    { name: 'password', label: '密码', input: { type: 'number' } },
   ]);
-  return <Form value={formData} fileds={fileds} />;
+
+  const [errors, setErrors] = useState({});
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const rules = [
+      { key: 'username', required: true },
+      {
+        key: 'username',
+        minLength: 8,
+        maxLength: 16,
+      },
+      {
+        key: 'username',
+        pattern: /^[A-Za-z0-9]+$/,
+      },
+      {
+        key: 'password',
+        required: true,
+      },
+    ];
+    const errors = Validator(formData, rules);
+    if (noError(errors)) {
+      // 没错
+    }
+    setErrors(errors);
+    // console.log('errors', errors);
+  };
+  return (
+    <Form
+      value={formData}
+      fileds={fileds}
+      buttons={
+        <Fragment>
+          <button type="submit">提交</button>
+          <button>返回</button>
+        </Fragment>
+      }
+      errors={errors}
+      onChange={(newValue) => setFormData(newValue)}
+      onSubmit={onSubmit}
+    />
+  );
 };
 
 export default FormExample;
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { ReactFragment } from 'react';
+
+export interface FormValue {
+  [K: string]: any;
+}
 
 interface Props {
-  value: { [k: string]: any };
+  value: FormValue;
   fileds: Array<{ name: string; label: string; input: { type: string } }>;
+  buttons: ReactFragment;
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+  onChange: (value: FormValue) => void;
+  errors: { [K: string]: string[] };
 }
+
 const Form: React.FunctionComponent<Props> = (props) => {
+  const formData = props.value;
+
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    props.onSubmit(e);
+  };
+
+  // const onInputChange = (name: string, value: string) => {
+  //   console.log(name, value);
+  // };
+
+  const onInputChange = (name: string, e: React.ChangeEvent<HTMLInputElement>) => {
+    const newFormValue = { ...formData, [name]: e.target.value };
+    props.onChange(newFormValue);
+  };
+
   return (
-    <div>
-      {props.fileds.map(f => 
-        <div>
+    <form onSubmit={onSubmit}>
+      {props.fileds.map((f) => (
+        <div key={f.name}>
           {f.label}
-          <input type={f.input.type}/>
+          <input
+            type={f.input.type}
+            value={formData[f.name]}
+            // onChange={(e) => onInputChange(f.name, e.target.value)}
+            onChange={onInputChange.bind(null, f.name)}
+          />
+          <div>{props.errors[f.name]}</div>
         </div>
-      )}
-    </div>
-  )
+      ))}
+      <div>{props.buttons}</div>
+    </form>
+  );
 };
 
 export default Form;
