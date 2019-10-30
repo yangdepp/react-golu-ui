64a07989f2519579c0551dae73e5c13489c21bc2
yangdepu <yangdepu@zdsoft.cn>
2019-10-30 18:27:29 +0800

    drag scrollbar

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,11 @@
-import React, { HTMLAttributes, UIEventHandler, useState, useEffect, useRef } from 'react';
+import React, {
+  HTMLAttributes,
+  UIEventHandler,
+  useState,
+  useEffect,
+  useRef,
+  MouseEventHandler,
+} from 'react';
 import './scroll.scss';
 import scrollbarWidth from './scrollbar-width';
 
@@ -7,7 +14,21 @@ interface Props extends HTMLAttributes<HTMLDivElement> {}
 const Scroll: React.FunctionComponent<Props> = (props) => {
   const { children, ...rest } = props;
   const [barHeight, setBarHeight] = useState(0);
-  const [barTop, setBarTop] = useState(0);
+  const [barTop, _setBarTop] = useState(0);
+
+  const setBarTop = (number: number) => {
+    if (number < 0) {
+      return;
+    }
+    const { current } = containerRef;
+    const scrollHeight = current!.scrollHeight;
+    const viewHeight = current!.getBoundingClientRect().height;
+    const maxBarTop = ((scrollHeight - viewHeight) * viewHeight) / scrollHeight;
+    if (number > maxBarTop) {
+      return;
+    }
+    _setBarTop(number);
+  };
 
   const onScroll: UIEventHandler = (e) => {
     const { current } = containerRef;
@@ -17,6 +38,24 @@ const Scroll: React.FunctionComponent<Props> = (props) => {
     setBarTop((scrollTop * viewHeight) / scrollHeight);
   };
 
+  const firstYRef = useRef(0);
+  const draggingRef = useRef(false);
+  const firstBarTopRef = useRef(0);
+
+  const onMouseDownBar: MouseEventHandler = (e) => {
+    draggingRef.current = true;
+    firstYRef.current = e.clientY;
+    firstBarTopRef.current = barTop;
+  };
+  const onMouseMoveBar = (e: MouseEvent) => {
+    if (draggingRef.current) {
+      const delta = e.clientY - firstYRef.current;
+      setBarTop(firstBarTopRef.current + delta);
+    }
+  };
+  const onMouseUpBar = () => {
+    draggingRef.current = false;
+  };
   const containerRef = useRef<HTMLDivElement>(null);
   // 页面渲染后算出滚动条的高度
   useEffect(() => {
@@ -24,7 +63,14 @@ const Scroll: React.FunctionComponent<Props> = (props) => {
     const viewHeight = containerRef.current!.getBoundingClientRect().height;
     setBarHeight((viewHeight * viewHeight) / scrollHeight);
   }, []);
-
+  useEffect(() => {
+    document.addEventListener('mouseup', onMouseUpBar);
+    document.addEventListener('mousemove', onMouseMoveBar);
+    return () => {
+      document.removeEventListener('mouseup', onMouseUpBar);
+      document.removeEventListener('mousemove', onMouseMoveBar);
+    };
+  }, []);
   return (
     <div className="golu-scroll" {...rest}>
       <div
@@ -39,6 +85,7 @@ const Scroll: React.FunctionComponent<Props> = (props) => {
         <div
           className="golu-scroll-bar"
           style={{ height: barHeight, transform: `translateY(${barTop}px)` }}
+          onMouseDown={onMouseDownBar}
         ></div>
       </div>
     </div>
